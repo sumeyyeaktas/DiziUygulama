ff6029e5699f0f2c2f2fd63b1397be22aa3241b1
sumeyyeaktas <56781355+sumeyyeaktas@users.noreply.github.com>
2019-10-22 10:49:43 +0300

    Update Program.cs
    Dizi içerisinde contains metodu ile arama yapma (Bensu)
    Her elemanın karesini alarak b dizisina aktaran , her elamanın kare kökünü alarak c dizisine aktaran , 
    b ve c dizilerinin toplamını d dizisine aktaran ve d dizisini ekranda gösterme (Umut)

--- DiziUygulama/DiziUygulama/Program.cs
+++ DiziUygulama/DiziUygulama/Program.cs
@@ -21,6 +21,33 @@ namespace DiziUygulama
                 Console.WriteLine(a[i]);
             }
             
+            //Dizi içerisinde contains metodu ile arama yapma (Bensu)
+            Console.Write("Aranacak sayıyı giriniz= ");
+            int aranan2 = Convert.ToInt32(Console.ReadLine());
+
+            if (a.Contains(aranan2) == true)
+            {
+                Console.WriteLine("Aranan değer mevcut");
+            }
+            else
+            {
+                Console.WriteLine("Aranan değer mevcut değil");
+            }
+            
+            //her elemanın karesini alarak b dizisina aktaran , her elamanın kare kökünü alarak c dizisine aktaran , 
+            //b ve c dizilerinin toplamını d dizisine aktaran ve d dizisini ekranda gösteren kod (Umut)
+            int[] b = new int[10];
+            double[] c = new double[10];
+            double[] d = new double[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                b[i] = a[i] * a[i];
+                c[i] = Math.Sqrt(a[i]);
+                d[i] = b[i] + c[i];
+                Console.WriteLine(d[i].ToString());
+            }
+            
             Console.ReadKey();
         }
     }
